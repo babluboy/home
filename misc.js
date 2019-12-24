016271ad9376b5a126f9c270dff4e200e884165a
Siddhartha Das <bablu.boy@gmail.com>
2019-12-24 12:33:24 +0530

    added license page and converted payment to radio buttons

--- misc.js
+++ misc.js
@@ -4,131 +4,132 @@
         }
         //set Category and Sub Category based on description
         function predict_category_info(data){
-        	if(data.toLowerCase().includes("nature")){
+                data = data.toLowerCase();
+        	if(data.includes("nature")){
         		document.getElementById("Category").value = "Grocery";
         		document.getElementById("Sub_Category").value = "Non Veg";
         	}
-        	if(data.toLowerCase().includes("vegetable")){
+        	if(data.includes("vegetable")){
         		document.getElementById("Category").value = "Grocery";
         		document.getElementById("Sub_Category").value = "Vegetables";
         	}
-        	if(data.toLowerCase().includes("dmart") || data.toLowerCase().includes("carry") || data.toLowerCase().includes("bread")){
+        	if(data.includes("dmart") || data.includes("carry") || data.includes("bread")){
         		document.getElementById("Category").value = "Grocery";
         		document.getElementById("Sub_Category").value = "Food and Household";
         	}
-        	if(data.toLowerCase().includes("milk")){
+        	if(data.includes("milk")){
         		document.getElementById("Category").value = "Grocery";
         		document.getElementById("Sub_Category").value = "Milk";
         	}
-        	if(data.toLowerCase().includes("fruit")){
+        	if(data.includes("fruit") || data.includes("apple") || data.includes("orange") || data.includes("bannanas")){
         		document.getElementById("Category").value = "Grocery";
         		document.getElementById("Sub_Category").value = "Fruits";
         	}
-        	if(data.toLowerCase().includes("fish")){
+        	if(data.includes("fish")){
         		document.getElementById("Category").value = "Grocery";
         		document.getElementById("Sub_Category").value = "Non Veg";
         	}
-        	if(data.toLowerCase().includes("egg")){
+        	if(data.includes("egg")){
         		document.getElementById("Category").value = "Grocery";
         		document.getElementById("Sub_Category").value = "Non Veg";
         	}
-        	if(data.toLowerCase().includes("poda")){
+        	if(data.includes("poda")){
         		document.getElementById("Category").value = "Outside Food";
         		document.getElementById("Sub_Category").value = "Chicken Poda";
         	}
-        	if(data.toLowerCase().includes("brijwasi") || data.toLowerCase().includes("ramas")){
+        	if(data.includes("brijwasi") || data.includes("ramas")){
         		document.getElementById("Category").value = "Outside Food";
         		document.getElementById("Sub_Category").value = "Snacks";
         	}
-        	if(data.toLowerCase().includes("swiggy") || data.toLowerCase().includes("zomato") || data.toLowerCase().includes("domino") || data.toLowerCase().includes("donald") || data.toLowerCase().includes("pizza") || data.toLowerCase().includes("hotel")){
+        	if(data.includes("swiggy") || data.includes("zomato") || data.includes("domino") || data.includes("donald") || data.includes("pizza") || data.includes("hotel")){
         		document.getElementById("Category").value = "Outside Food";
         		document.getElementById("Sub_Category").value = "Meals";
         	}
-        	if(data.toLowerCase().includes("gas")){
+        	if(data.includes("gas")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Pipe Gas";
         	}
-        	if(data.toLowerCase().includes("sky")){
+        	if(data.includes("sky")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Dish TV";
         	}
-        	if(data.toLowerCase().includes("mtnl")){
+        	if(data.includes("mtnl")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Broadband";
         	}
-        	if(data.toLowerCase().includes("electricity")){
+        	if(data.includes("electricity")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Electricity";
         	}
-        	if(data.toLowerCase().includes("gayatri") || data.toLowerCase().includes("maintenence")){
+        	if(data.includes("gayatri") || data.includes("maintenence")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Society Maintenence";
         	}
-        	if(data.toLowerCase().includes("emi") || data.toLowerCase().includes("loan")){
+        	if(data.includes("emi") || data.includes("loan")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Epsilon Home Loan";
         	}
-        	if(data.toLowerCase().includes("school") || data.toLowerCase().includes("fees")){
+        	if(data.includes("school") || data.includes("fees")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "School Fees";
         	}
-        	if(data.toLowerCase().includes("maid")){
+        	if(data.includes("maid")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "House Work";
         	}
-        	if(data.toLowerCase().includes("apy") || data.toLowerCase().includes("equity") || data.toLowerCase().includes("share") || data.toLowerCase().includes("lic")){
+        	if(data.includes("apy") || data.includes("equity") || data.includes("share") || data.includes("lic")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Savings";
         	}
-        	if(data.toLowerCase().includes("animal") || data.toLowerCase().includes("pintu")){
+        	if(data.includes("animal") || data.includes("pintu")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Donation";
         	}
-        	if(data.toLowerCase().includes("charge")){
+        	if(data.includes("charge")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Mobile";
         	}
-        	if(data.toLowerCase().includes("salary") || data.toLowerCase().includes("rent")){
+        	if(data.includes("salary") || data.includes("rent")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Income";
         	}
-        	if(data.toLowerCase().includes("football")){
+        	if(data.includes("football")){
         		document.getElementById("Category").value = "Monthly";
         		document.getElementById("Sub_Category").value = "Kids Clubs";
         	}
-        	if(data.toLowerCase().includes("toll")){
+        	if(data.includes("toll")){
         		document.getElementById("Category").value = "Travel";
         		document.getElementById("Sub_Category").value = "Mira Road Toll";
         	}
-        	if(data.toLowerCase().includes("ola") || data.toLowerCase().includes("uber")){
+        	if(data.includes("ola") || data.includes("uber")){
         		document.getElementById("Category").value = "Travel";
         		document.getElementById("Sub_Category").value = "Cabs";
         	}
-        	if(data.toLowerCase().includes("park") || data.toLowerCase().includes("petrol")){
+        	if(data.includes("park") || data.includes("petrol")){
         		document.getElementById("Category").value = "Travel";
         		document.getElementById("Sub_Category").value = "Vehicle Costs";
         	}
-        	if(data.toLowerCase().includes("hair")){
+        	if(data.includes("hair")){
         		document.getElementById("Category").value = "Life Needs";
         		document.getElementById("Sub_Category").value = "Haircut";
         	}
-        	if(data.toLowerCase().includes("clap")){
+        	if(data.includes("clap")){
         		document.getElementById("Category").value = "Life Needs";
         		document.getElementById("Sub_Category").value = "Salon";
         	}
-        	if(data.toLowerCase().includes("brows")){
+        	if(data.includes("brows")){
         		document.getElementById("Category").value = "Life Needs";
         		document.getElementById("Sub_Category").value = "Salon";
         	}
-        	if(data.toLowerCase().includes("medicine") || data.toLowerCase().includes("noble")){
+        	if(data.includes("medicine") || data.includes("noble")){
         		document.getElementById("Category").value = "Life Needs";
         		document.getElementById("Sub_Category").value = "Medicines";
         	}
-        	if(data.toLowerCase().includes("central") || data.toLowerCase().includes("shopper") || data.toLowerCase().includes("dress")){
+        	if(data.includes("central") || data.includes("shopper") || data.includes("dress")){
         		document.getElementById("Category").value = "Shopping";
         		document.getElementById("Sub_Category").value = "Clothes";
         	}
-        	if(data.toLowerCase().includes("movie")){
+        	if(data.includes("movie")){
         		document.getElementById("Category").value = "Shopping";
         		document.getElementById("Sub_Category").value = "Entertainment";
         	}
